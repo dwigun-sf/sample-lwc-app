5e96972a9a6e94727e89d8ffb87f38209d5c4f1c
dpalla <dpalla@salesforce.com>
2020-10-20 15:40:13 +0530

    added card and nav

---
+++ src/client/modules/my/card/card.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class CompositionParent extends LightningElement {
+    contact = {
+        Name: 'Salesforce',
+        Title: 'Digital delivery',
+        Picture: '/resources/salesforce.png'
+    };
+}
