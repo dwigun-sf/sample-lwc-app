7e154b377674371b0f347c782989873210d5bc76
dpalla <dpalla@salesforce.com>
2020-12-16 11:05:38 +0530

    test

--- src/client/modules/my/app/__tests__/lwc.js
+++ src/client/modules/my/app/__tests__/lwc.js
@@ -1,10 +1,13 @@
-let i, j, k, l, m;
+let i, j, k, l, m, n;
 for (i = 0; i < 5; i++) {
     for (j = 0; j < 5; j++) {
         for (k = 0; k < 5; k++) {
             for (l = 0; l < 5; l++) {
                 for (m = 0; m < 5; m++) {
                     console.log(i + j + k);
+                    for (n = 0; n < 5; n++) {
+                        console.log(n);
+                    }
                 }
             }
         }
